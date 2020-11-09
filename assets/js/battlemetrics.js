1688bc2cb741360da2940646b46220fdfc2984c9
Schwaitz <justinschwaitzberg@gmail.com>
2020-11-08 23:29:22 -0500

    update

--- assets/js/battlemetrics.js
+++ assets/js/battlemetrics.js
@@ -1,14 +1,62 @@
 var servers = {
-    Ragnarok: {name: 'Ragnarok', code: 'hgnom', src: 'https://cdn.battlemetrics.com/b/standardVertical/6654616.html?foreground=%23EEEEEE&linkColor=%231185ec&lines=%23333333&background=%23222222&chart=players%3A24H&chartColor=%23FF0700&maxPlayersHeight=300'},
-    Extinction: {name: 'Extinction', code: 'rwocd',  src: 'https://cdn.battlemetrics.com/b/standardVertical/6654623.html?foreground=%23EEEEEE&linkColor=%231185ec&lines=%23333333&background=%23222222&chart=players%3A24H&chartColor=%23FF0700&maxPlayersHeight=300'},
-    TheIsland: {name: 'TheIsland', code: 'gjhhq',  src: 'https://cdn.battlemetrics.com/b/standardVertical/6654622.html?foreground=%23EEEEEE&linkColor=%231185ec&lines=%23333333&background=%23222222&chart=players%3A24H&chartColor=%23FF0700&maxPlayersHeight=300'},
-    Genesis: {name: 'Genesis', code: 'gircb',  src: 'https://cdn.battlemetrics.com/b/standardVertical/6654620.html?foreground=%23EEEEEE&linkColor=%231185ec&lines=%23333333&background=%23222222&chart=players%3A24H&chartColor=%23FF0700&maxPlayersHeight=300'},
-    Aberration: {name: 'Aberration', code: 'rzdmv',  src: 'https://cdn.battlemetrics.com/b/standardVertical/6654620.html?foreground=%23EEEEEE&linkColor=%231185ec&lines=%23333333&background=%23222222&chart=players%3A24H&chartColor=%23FF0700&maxPlayersHeight=300'},
-    CrystalIsles: {name: 'Crystal Isles', code: 'irttx',  src: 'https://cdn.battlemetrics.com/b/standardVertical/7274534.html?foreground=%23EEEEEE&linkColor=%231185ec&lines=%23333333&background=%23222222&chart=players%3A24H&chartColor=%23FF0700&maxPlayersHeight=300'},
-    ScorchedEarth: {name: 'Scorched Earth', code: 'otaip',  src: 'https://cdn.battlemetrics.com/b/standardVertical/6654613.html?foreground=%23EEEEEE&linkColor=%231185ec&lines=%23333333&background=%23222222&chart=players%3A24H&chartColor=%23FF0700&maxPlayersHeight=300'},
-    TheCenter: {name: 'The Center', code: 'hqsja',  src: 'https://cdn.battlemetrics.com/b/standardVertical/6654615.html?foreground=%23EEEEEE&linkColor=%231185ec&lines=%23333333&background=%23222222&chart=players%3A24H&chartColor=%23FF0700&maxPlayersHeight=300'},
-    Valguero: {name: 'Valguero', code: 'dmmpe',  src: 'https://cdn.battlemetrics.com/b/standardVertical/6654614.html?foreground=%23EEEEEE&linkColor=%231185ec&lines=%23333333&background=%23222222&chart=players%3A24H&chartColor=%23FF0700&maxPlayersHeight=300'},
+    Ragnarok: {
+        name: "Ragnarok",
+        code: "ynsmd",
+        src: 'https://cdn.battlemetrics.com/b/standardVertical/6654616.html?foreground=%23EEEEEE&linkColor=%231185ec&lines=%23333333&background=%23222222&chart=players%3ART&chartColor=%23FF0700&showPlayers=1&maxPlayersHeight='
+    },
+    Extinction: {
+        name: "Extinction",
+        code: "amisf",
+        src: 'https://cdn.battlemetrics.com/b/standardVertical/6654623.html?foreground=%23EEEEEE&linkColor=%231185ec&lines=%23333333&background=%23222222&chart=players%3ART&chartColor=%23FF0700&showPlayers=1&maxPlayersHeight='
+    },
+    TheIsland: {
+        name: "The Island",
+        code: "nxogt",
+        src: 'https://cdn.battlemetrics.com/b/standardVertical/6654622.html?foreground=%23EEEEEE&linkColor=%231185ec&lines=%23333333&background=%23222222&chart=players%3ART&chartColor=%23FF0700&showPlayers=1&maxPlayersHeight='
+    },
+    Genesis: {
+        name: "Genesis",
+        code: "upywn",
+        src: 'https://cdn.battlemetrics.com/b/standardVertical/6654621.html?foreground=%23EEEEEE&linkColor=%231185ec&lines=%23333333&background=%23222222&chart=players%3ART&chartColor=%23FF0700&showPlayers=1&maxPlayersHeight='
+    },
+    Aberration: {
+        name: "Aberration",
+        code: "prkox",
+        src: 'https://cdn.battlemetrics.com/b/standardVertical/8813445.html?foreground=%23EEEEEE&linkColor=%231185ec&lines=%23333333&background=%23222222&chart=players%3ART&chartColor=%23FF0700&showPlayers=1&maxPlayersHeight='
+    },
+    CrystalIsles: {
+        name: "Crystal Isles",
+        code: "tdrbm",
+        src: 'https://cdn.battlemetrics.com/b/standardVertical/7274534.html?foreground=%23EEEEEE&linkColor=%231185ec&lines=%23333333&background=%23222222&chart=players%3ART&chartColor=%23FF0700&showPlayers=1&maxPlayersHeight='
+    },
+    ScorchedEarth: {
+        name: "Scorched Earth",
+        code: "zxyxi",
+        src: 'https://cdn.battlemetrics.com/b/standardVertical/6654613.html?foreground=%23EEEEEE&linkColor=%231185ec&lines=%23333333&background=%23222222&chart=players%3ART&chartColor=%23FF0700&showPlayers=1&maxPlayersHeight='
+    },
+    TheCenter: {
+        name: "The Center",
+        code: "ciijn",
+        src: 'https://cdn.battlemetrics.com/b/standardVertical/6654615.html?foreground=%23EEEEEE&linkColor=%231185ec&lines=%23333333&background=%23222222&chart=players%3ART&chartColor=%23FF0700&showPlayers=1&maxPlayersHeight='
+    },
+    Valguero: {
+        name: "Valguero",
+        code: "jesdc",
+        src: 'https://cdn.battlemetrics.com/b/standardVertical/6654614.html?foreground=%23EEEEEE&linkColor=%231185ec&lines=%23333333&background=%23222222&chart=players%3ART&chartColor=%23FF0700&showPlayers=1&maxPlayersHeight='
+    },
 
+};
+
+var serverIds = {
+    "Ragnarok": "6654616",
+    "The Center": "6654615",
+    "Valguero": "6654614",
+    "Crystal Isles": "7274534",
+    "Genesis": "6654621",
+    "The Island": "6654622",
+    "Extinction": "6654623",
+    "Scorched Earth": "6654613",
+    "Aberration": "8813445",
 };
 
 
@@ -30,18 +78,13 @@ $(document).ready(function () {
 
 
     $(":button").click(function () {
-
         if ($(this).hasClass("bm-button")) {
             var serverName = $(this).prop("id");
-
+            handleMapChange(serverName);
             handleBMLoading(servers[serverName]["code"], servers[serverName]["src"]);
         }
 
-
-
     });
-
-
 
     window.addEventListener('message', function (e) {
         if (e.data.uid && e.data.type === 'sizeUpdate') {
@@ -53,6 +96,35 @@ $(document).ready(function () {
 
 });
 
+
+function handleMapChange(serverName) {
+    var serverID = serverIds[serverName];
+    $("#selected-map").text(serverName);
+
+    // $.getJSON("https://api.battlemetrics.com/servers/" + serverID + "?include=player", function (obj) {
+    //     // var player_count = parseInt(obj["data"]["attributes"]["players"]);
+    //     // var map = obj["data"]["attributes"]["details"]["map"];
+    //
+    //     $("#selected-map").text(serverName);
+    //
+    //     $("#player-list-header").text("Player List");
+    //     $("#player-list-ul").innerHTML = "";
+    //
+    //     var players = [];
+    //
+    //     for (var i = 0; i < obj["included"].length; i++) {
+    //         if (obj["included"][i]["type"] === "player") {
+    //             players.push(obj["included"][i]["attributes"]["name"])
+    //         }
+    //     }
+    //
+    //     for (var i = 0; i < players.length; i++) {
+    //         $("#player-list-ul").append("<li id='player-list-" + players[i] + "'>" + players[i] + "</li>");
+    //     }
+    //
+    //     console.log(obj)
+    // });
+}
 
 
 function handleBMLoading(code, src) {
--- assets/js/resources.js
+++ assets/js/resources.js
@@ -1,70 +1,155 @@
 var components = {
-    metal: {name: 'Metal', color: 'grey', display: 'true'},
-    richMetal: {name: 'Rich Metal', color: 'GoldenRod', display: 'true'},
-    crystal: {name: 'Crystal', color: 'PaleTurquoise', display: 'true'},
-    obsidian: {name: 'Obsidian', color: 'BlueViolet', display: 'false'},
-    oil: {name: 'Oil', color: 'black', display: 'false'},
-    silica: {name: 'Silica Pearls', color: 'white', display: 'false'},
-    // riverRock: {name: 'River Rock', color: 'Bisque', display: 'true'},
-    salt: {name: 'Salt', color: 'AliceBlue', display: 'false'},
-    sulfur: {name: 'Sulfur', color: 'Yellow', display: 'false'},
-    // cactus: {name: 'Cactus', color: 'OliveDrab', display: 'true'}
+    crystal: {name: 'Crystal', color: 'PaleTurquoise', borderColor: 'black', display: 'true'},
+
+    metal: {name: 'Metal', color: 'silver', borderColor: 'black', display: 'true'},
+    richMetal: {name: 'Rich Metal', color: 'GoldenRod', borderColor: 'black', display: 'true'},
+    underwaterMetal: {name: 'Underwater Metal', color: 'teal', borderColor: 'black', display: 'false'},
+
+    obsidian: {name: 'Obsidian', color: 'BlueViolet', borderColor: 'white', display: 'false'},
+
+    oil: {name: 'Oil', color: 'black', borderColor: 'black', display: 'false'},
+    oilRock: {name: 'Oil Rock', color: '#3B3131', borderColor: 'white', display: 'false'},
+
+    oilVeins: {name: 'Oil Vein', color: '#3B3131', borderColor: 'white', display: 'false'},
+    oilVeins2: {name: 'Oil Vein 2', color: '#3B3131', borderColor: 'white', display: 'false'},
+    waterVeins: {name: 'Water Vein', color: 'cyan', borderColor: 'white', display: 'false'},
+
+    silica: {name: 'Silica Pearls', color: 'pink', borderColor: 'black', display: 'false'},
+    blackPearl: {name: 'Black Pearl', color: 'magenta', borderColor: 'black', display: 'false'},
+    salt: {name: 'Raw Salt', color: '#679dae', borderColor: 'black', display: 'false'},
+    rockarrot: {name: 'Rockarrot', color: '#3BD319', borderColor: 'black', display: 'false'},
+    savoroot: {name: 'Savoroot', color: '2B9111', borderColor: 'black', display: 'false'},
+    rareFlower: {name: 'Rare Flower', color: '#ADD8E6', borderColor: 'black', display: 'false'},
+    sulfur: {name: 'Sulfur', color: 'Yellow', borderColor: 'black', display: 'false'},
+    clay: {name: 'Clay', color: '#FFD800', borderColor: 'black', display: 'false'},
+    cactus: {name: 'Cactus', color: 'lightgreen', borderColor: 'green', display: 'false'},
+
+    richDust: {name: 'Rich Dust', color: '#E577DF', borderColor: 'black', display: 'false'},
+
+    riverRock: {name: 'River Rock', color: 'brown', borderColor: 'black', display: 'false'},
+
+    polymer: {name: 'Polymer', color: '#678396', borderColor: 'black', display: 'false'},
+    elementOre: {name: 'Element Ore', color: '#E577DF', borderColor: 'black', display: 'false'},
+    keratin: {name: 'Keratin', color: '#CBB195', borderColor: 'black', display: 'false'},
+
+    greenGem: {name: 'Green Gem', color: '#60AF33', borderColor: 'black', display: 'false'},
+    blueGem: {name: 'Blue Gem', color: '#5091CD', borderColor: 'black', display: 'false'},
+    redGem: {name: 'Red Gem', color: '#E26F78', borderColor: 'black', display: 'false'},
+
+    wyvernNests: {name: 'Wyvern Nest', color: '#80456D', borderColor: 'black', display: 'false'},
+    iceWyvernNests: {name: 'Ice Wyvern Nest', color: '#4a7880', borderColor: 'black', display: 'false'}
+};
+
+
+var map_resources = {
+    "TheIsland": ["crystal", "metal", "richMetal", "obsidian", "silica", "oil", "riverRock"],
+    "TheCenter": ["crystal", "metal", "richMetal", "obsidian", "silica", "oil", "riverRock"],
+    "ScorchedEarth": ["oilVeins", "oilVeins2", "waterVeins", "wyvernNests", "salt", "sulfur", "crystal", "metal", "richMetal", "obsidian", "silica", "cactus"],
+    "Ragnarok": ["oilVeins", "oilVeins2", "waterVeins", "wyvernNests", "iceWyvernNests", "salt", "sulfur", "crystal", "metal", "richMetal", "underwaterMetal", "obsidian", "silica", "oil", "riverRock", "cactus", "blackPearl", "rareFlower", "rockarrot", "savoroot", "clay",],
+    "Aberration": ["gasVeins", "drakeNests", "crystal", "metal", "richMetal", "obsidian", "silica", "oil", "riverRock", "blackPearl", "polymer", "elementOre", "keratin", "blueGem", "greenGem", "redGem"],
+    "Extinction": ["crystal", "metal", "richMetal", "obsidian", "oil", "riverRock"],
+    "Crystal": ["oilVeins", "oilVeins2", "waterVeins", "gasVeins", "drakeNests", "wyvernNests", "salt", "sulfur", "crystal", "metal", "richMetal", "obsidian", "silica", "oil", "riverRock", "cactus", "elementOre", "keratin", "greenGem", "redGem",],
+    "Genesis": ["oilVeins", "salt", "sulfur", "crystal", "metal", "richMetal", "obsidian", "oil", "riverRock", "blueGem", "greenGem", "redGem"]
+    // "Valguero": [],
 };
 
 var currentMap = "Ragnarok";
 
-
 $(document).ready(function () {
 
-
     // $("#current-map").text(currentMap);
-    loadMap(currentMap, components);
 
     for (var componentName in components) {
         var component = components[componentName];
         var startChecked = component["display"];
 
         if (startChecked == "true") {
-            $("#boxes").append("<input class='form-check-input res-check' type='checkbox' value='' id='" + componentName + "' name='checkbox-stacked' checked><label class='form-check-label res-label' for='" + componentName + "'>" + component["name"] + "</label><br/>")
+            $("#boxes").append("<input class='form-check-input res-check' type='checkbox' value='' id='" + componentName + "' name='checkbox-stacked' checked><label class='form-check-label res-label' id='" + componentName + "-label' for='" + componentName + "'>" + component["name"] + "</label><br/>")
         } else {
-            $("#boxes").append("<input class='form-check-input res-check' type='checkbox' value='' id='" + componentName + "' name='checkbox-stacked'><label class='form-check-label res-label' for='" + componentName + "'>" + component["name"] + "</label><br/>")
+            $("#boxes").append("<input class='form-check-input res-check' type='checkbox' value='' id='" + componentName + "' name='checkbox-stacked'><label class='form-check-label res-label' id='" + componentName + "-label' for='" + componentName + "'>" + component["name"] + "</label><br/>")
         }
-
     }
 
+    loadMap(currentMap);
 
     $(":button").click(function () {
         if ($(this).hasClass("map-button")) {
 
             if ($(this).text().localeCompare(currentMap) !== 0) {
                 // $("#current-map").text($(this).text());
-                loadMap(this.dataset.map, components);
+                // $("#output").empty();
+
+                loadMap(this.dataset.map);
                 currentMap = this.dataset.map
 
             }
         }
     });
 
-
     $('input[name=checkbox-stacked]').change(function () {
         var res_name = $(this).prop("id");
 
         if ($(this).is(':checked')) {
             components[res_name]["display"] = "true";
-            loadMap(currentMap, components);
+            $(".dot-" + res_name).each(function (i, obj) {
+                $(this).css("visibility", "visible");
+            });
+            // loadMap(currentMap);
         } else {
             components[res_name]["display"] = "false";
-            loadMap(currentMap, components);
+            $(".dot-" + res_name).each(function (i, obj) {
+                $(this).css("visibility", "hidden");
+            });
+            // loadMap(currentMap);
         }
     });
 
 });
 
 
-function loadMap(name, components) {
+function loadLegend(name) {
+    $(".res-check").each(function (i, obj) {
+        var res_name = $(this).prop("id");
+        if (map_resources[name].includes(res_name)) {
+            $(this).show();
+            $(this).next().show()
+            // $(this).removeClass("visually-hidden");
+            // $(this).css("display", "inline");
+            // $(this).css("visibility", "visible");
+        } else {
+            $(this).hide();
+            $(this).next().hide()
+            // $(this).addClass("visually-hidden");
+            // $(this).css("display", "none");
+            // $(this).css("visibility", "hidden");
+        }
+    });
+
+    $(".res-label").each(function (i, obj) {
+        var res_name = $(this).prop("id").replace("-label", "");
+        if (map_resources[name].includes(res_name)) {
+            $(this).show();
+            $(this).next().show()
+            // $(this).removeClass("visually-hidden");
+            // $(this).css("display", "inline");
+            // $(this).css("visibility", "visible");
+        } else {
+            $(this).hide();
+            $(this).next().hide()
+            // $(this).addClass("visually-hidden");
+            // $(this).css("display", "none");
+            // $(this).css("visibility", "hidden");
+        }
+    });
+}
+
+
+function loadMap(name) {
     (function () {
+        // $("#output").append("<img height='800' id='mapImage' width='800'>");
+        loadLegend(name);
         var map = name;
-        fetch('assets/maps/json/' + map + '/map.json', {cache: 'reload'}).then(function (response) {
+        fetch('assets/maps/json/' + map + '/map.min.json', {cache: 'reload'}).then(function (response) {
             return response.json();
         }).then(function (data) {
             document.getElementById('mapImage').src = 'assets/maps/imgs/' + map + ".jpg";
@@ -76,31 +161,42 @@ function loadMap(name, components) {
 
             for (var componentName in components) {
                 var component = components[componentName];
-                if (component["display"] === "true") {
-                    if (data[componentName]) {
-                        for (var node of data[componentName]) {
-                            var lat = node.lat;
-                            var lon = node.long;
-                            var mapsize = 800;
-                            var ms = 7;
-                            var mt = 'lat ' + lat + ', lon ' + lon;
-                            var borderCoords = {t: 0.0, r: 100.0, b: 100.0, l: 0.0};
+                if (data[componentName]) {
+                    for (var node of data[componentName]) {
+                        var latShift = parseFloat(data["latShift"]);
+                        var longShift = parseFloat(data["longShift"]);
+                        var latScale = parseInt(data["latScale"]);
+                        var longScale = parseInt(data["longScale"]);
 
-                            var div = document.createElement('div');
-                            div.style.position = 'absolute';
-                            div.style.lineHeight = 0;
-                            div.style.left = 100 * ((lon - borderCoords.l) / (borderCoords.r - borderCoords.l) - ms / (2 * mapsize)) + '%';
-                            div.style.top = 100 * ((lat - borderCoords.t) / (borderCoords.b - borderCoords.t) - ms / (2 * mapsize)) + '%';
-                            div.style.padding = 0;
-                            div.style.width = ms + 'px';
-                            div.style.height = ms + 'px';
-                            div.style.borderRadius = '50%';
-                            div.style.backgroundColor = component.color;
-                            div.style.border = '1px solid black';
-                            div.title = mt;
+                        var lat = node.lat;
+                        var lon = node.long;
+                        // var mapsize = 800;
+                        var mapsize = 800;
+                        var ms = 7;
+                        var mt = 'lat ' + lat + ', lon ' + lon;
+                        var borderCoords = {t: 0.0, r: 100.0, b: 100.0, l: 0.0};
+                        // var borderCoords = {t: 0.0, r: 100.0, b: 100.0, l: 0.0};
 
-                            output.insertBefore(div, output.firstChild);
+                        var div = document.createElement('div');
+                        div.className = "res-dot dot-" + componentName;
+                        if (component["display"] === "true") {
+                            div.style.visibility = "visible";
+                        } else {
+                            div.style.visibility = "hidden";
                         }
+                        div.style.position = 'absolute';
+                        div.style.lineHeight = 0;
+                        div.style.left = 100 * ((lon - borderCoords.l) / (borderCoords.r - borderCoords.l) - ms / (2 * mapsize)) + '%';
+                        div.style.top = 100 * ((lat - borderCoords.t) / (borderCoords.b - borderCoords.t) - ms / (2 * mapsize)) + '%';
+                        div.style.padding = 0;
+                        div.style.width = ms + 'px';
+                        div.style.height = ms + 'px';
+                        div.style.borderRadius = '50%';
+                        div.style.backgroundColor = component.color;
+                        div.style.border = '1px solid ' + component.borderColor;
+                        div.title = mt;
+
+                        output.insertBefore(div, output.firstChild);
                     }
                 }
             }
